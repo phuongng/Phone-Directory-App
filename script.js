ab5c0220a1d62befa6c106a33edb5f052a8790ac
phuongng <78459468+phuongng@users.noreply.github.com>
2022-07-13 11:50:46 -0400

    update notfound result message

--- script.js
+++ script.js
@@ -115,24 +115,31 @@ function searchFunction() {
 	// Declare variables
 	var input, filter, table, tr, td, i, txtValue;
 	input = document.getElementById("mySearchInput");
-	filter = input.value.toUpperCase();
+	filter = input.value;
 	table = document.getElementById("myTable");
 	tr = table.getElementsByTagName("tr");
 
 	// Loop through all table rows, and hide those who don't match the search query
 	for (i = 0; i < tr.length; i++) {
 		td = tr[i].getElementsByTagName("td")[1];
+
 		if (td) {
 			txtValue = td.textContent || td.innerText;
-			if (txtValue.toUpperCase().indexOf(filter) > -1) {
+			if (txtValue.indexOf(filter) > -1) {
 				tr[i].style.display = "";
+
 			} else {
 				tr[i].style.display = "none";
 				document.querySelector("#NoSearchFound").textContent = "No Results Found";
 			}
+		} else {
+			document.querySelector("#NoSearchFound").textContent = "";
 		}
+
 	}
 }
+
+
 
 
 
